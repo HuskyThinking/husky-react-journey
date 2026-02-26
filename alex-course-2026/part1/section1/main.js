d9a1a5de43faa6504e9d1ad598e89a34069cd9f8
li_dingxin <849627763@qq.com>
2026-02-26 09:55:39 +0800

    Update main.js

--- alex-course-2026/part1/section1/main.js
+++ alex-course-2026/part1/section1/main.js
@@ -11,18 +11,3 @@ function App() {
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 root.render(<App />);
-
-// {
-//     "workbench.colorTheme": "Dracula Theme",
-//     "workbench.iconTheme": "vscode-icons",
-//     "editor.fontSize": 16,
-//     "editor.fontFamily": "Fira Code",
-//     "editor.minimap.sectionHeaderFontSize": 12,
-//     "window.zoomLevel": 1.2,
-//     "editor.defaultFormatter": "esbenp.prettier-vscode",
-//     "files.autoSave": "onFocusChange",
-//     "prettier.semi": true,
-//     "prettier.singleQuote": true,
-//     "prettier.jsxSingleQuote": false,
-//     "editor.formatOnSave": true,
-// }
