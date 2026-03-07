a0f8fb34ecc9b72de9c1e6e767405ee9e859c3d3
Husky <849627763@qq.com>
2026-03-07 23:20:29 +0800

    alex-part2-section8

---
+++ alex-course-2026/part2/section8/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import useSWR from 'swr';
+
+function App() {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const adviceURL = 'https://api.adviceslip.com/advice';
+  const {
+    data,
+    error,
+    isLoading,
+    mutate: getAdvice,
+  } = useSWR(adviceURL, fetcher);
+  const [advice, setAdvice] = useState('');
+  return (
+    <main>
+      <h1>Advice App</h1>
+      <p>{isLoading ? 'isLoading...' : data.slip?.advice}</p>
+      <button disabled={isLoading} onClick={getAdvice}>
+        Get Advice
+      </button>
+    </main>
+  );
+}
+
+export default App;
