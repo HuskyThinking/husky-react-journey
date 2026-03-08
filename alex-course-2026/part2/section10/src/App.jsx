2d48d402df9188ad7ab001fa44d0d10df3545af9
Husky <849627763@qq.com>
2026-03-08 19:02:59 +0800

    alex-part2-section10

---
+++ alex-course-2026/part2/section10/src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+
+function App() {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const adviceURL = 'https://api.adviceslip.com/advice';
+  const {
+    data,
+    error,
+    isLoading,
+    mutate: getAdvice,
+  } = useSWR(adviceURL, fetcher);
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleString());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date().toLocaleString());
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <main>
+      <h1>Advice App</h1>
+      <span>{currentTime}</span>
+      <p>{isLoading ? 'isLoading...' : data.slip?.advice}</p>
+      <button disabled={isLoading} onClick={getAdvice}>
+        Get Advice
+      </button>
+    </main>
+  );
+}
+
+export default App;
